4e0973e64e71fe00a5c74c7a2b26af8f7150b755
Carlos <carlosdiazgirol@hotmail.com>
2024-02-13 10:05:46 +0100

    ready to work

---
+++ routes/users.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const router = express.Router();
+const User = require("../models/User.js");
+
+
+router.post("/create", async(req, res) => {
+    try {
+        const user = await User.create(req.body);
+        res.status(201).send(user);
+    } catch (error) {
+        console.error(error);
+        res
+            .status(500)
+            .send({ message: "There was a problem trying to create a user" });
+    }
+});
+
+router.get("/", async(req, res) => {
+    try {
+        const users = await User.find();
+        res.send(users);
+    } catch (error) {
+        console.error(error);
+    }
+});
+
+router.delete("/id/:_id", async(req, res) => {
+    try {
+        const user = await User.findByIdAndDelete(req.params._id);
+        res.send({ message: "user deleted", user });
+    } catch (error) {
+        console.error(error);
+        res
+            .status(500)
+            .send({ message: "There was a problem trying to delete a user" });
+    }
+})
+router.put("/id/:_id", async(req, res) => {
+        try {
+            const user = await User.findByIdAndUpdate(req.params._id, req.body, { new: true })
+            res.send({ message: "user successfully updated", user });
+        } catch (error) {
+            console.error(error);
+        }
+    }),
+
+    module.exports = router;
